d07e6c579d2127b424cc52d66eeaaeb2fed88449
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-01-05 21:31:23 -0500

    added some response handlers (GET, PUT, DELETE); added a public directory for static files

--- express/my-app/index.js
+++ express/my-app/index.js
@@ -5,8 +5,22 @@
   var express = require("express"),
     app = express();
 
+  app.use(express.static("public"));
+  
   app.get("/", function(request, response) {
     response.send("Hello World!");
+  });
+
+  app.post("/", function(request, response) {
+    response.send("Received a POST request");
+  });
+
+  app.put("/user", function(request, response) {
+    response.send("Received a PUT request at /user");
+  });
+
+  app.delete("/user", function(request, response) {
+    response.send("Received a DELETE request at /user");
   });
 
   app.listen(3000, function() {
